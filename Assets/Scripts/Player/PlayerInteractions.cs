1930b061280b018d321019c193c900baa3e05697
MB <62397807+M-Bujak@users.noreply.github.com>
2023-07-05 13:45:35 +0200

    Implemented basic shooting

---
+++ Assets/Scripts/Player/PlayerInteractions.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteractions : MonoBehaviour
+{
+    [SerializeField] private Transform _cornPrefab;
+    [SerializeField] private Transform _shootingPoint;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        Instantiate(_cornPrefab, _shootingPoint.position, Quaternion.identity);
+    }
+}
