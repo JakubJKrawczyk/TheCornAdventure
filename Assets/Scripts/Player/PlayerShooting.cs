0b184c0d4e99b7e5a875944cba45af3f648c52fc
MB <62397807+M-Bujak@users.noreply.github.com>
2023-07-05 14:09:34 +0200

    Implemented shooting delay

--- Assets/Scripts/Player/PlayerShooting.cs
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -7,16 +7,31 @@ public class PlayerInteractions : MonoBehaviour
     [SerializeField] private ProjectileSO _projectileSO;
     [SerializeField] private Transform _shootingPoint;
 
+    private float _timeOfLastShot = 0f;
+
     private void Update()
     {
         if(Input.GetKey(KeyCode.E))
         {
+            TryShoot();
+        }
+    }
+
+    private void TryShoot()
+    {
+        if(Time.time > _timeOfLastShot + _projectileSO.TimeBetweenShots)
+        {
             Shoot();
+        }
+        else
+        {
+            Debug.Log("Nope");
         }
     }
 
     private void Shoot()
     {
         Instantiate(_projectileSO.Prefab, _shootingPoint.position, Quaternion.identity);
+        _timeOfLastShot = Time.time;
     }
 }
