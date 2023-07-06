757e1edab691515fd4376dd13f956b5f189e0714
MB <62397807+M-Bujak@users.noreply.github.com>
2023-07-07 00:26:02 +0200

    Made Enemy class a base class

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -2,21 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public abstract class Enemy : MonoBehaviour
 {
     [Min(0)]
-    [field: SerializeField] public float InitialHP;
+    [field: SerializeField] protected float InitialHP;
     [Min(0)]
-    [field: SerializeField] public float DamageAmount;
+    [field: SerializeField] protected float DamageAmount;
 
-    private float CurrentHP;
+    protected float CurrentHP;
 
-    private void Start()
+    protected void Start()
     {
         CurrentHP = InitialHP;
     }
 
-    private void ReceiveDamage(float damageReceived)
+    protected void ReceiveDamage(float damageReceived)
     {
         CurrentHP -= damageReceived;
 
