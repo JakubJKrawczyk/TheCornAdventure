4dfffb1212d8ea54fb03b68ee91ec980692d9084
MB <62397807+M-Bujak@users.noreply.github.com>
2023-07-07 23:59:12 +0200

    Changed Enemy class into an independent EnemyHealth class

--- Assets/Scripts/Enemies/EnemyHealth.cs
+++ Assets/Scripts/Enemies/EnemyHealth.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Enemy : MonoBehaviour
+public abstract class EnemyHealth : MonoBehaviour
 {
     [Min(0)]
     [field: SerializeField] protected float _initialHP;
