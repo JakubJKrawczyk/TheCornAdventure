54f098ca32b780120580bc34a74ab54cea131ff8
MB <62397807+M-Bujak@users.noreply.github.com>
2023-07-08 00:04:43 +0200

    Made class not abstract, because it's not a base class

--- Assets/Scripts/Enemies/EnemyHealth.cs
+++ Assets/Scripts/Enemies/EnemyHealth.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class EnemyHealth : MonoBehaviour
+public class EnemyHealth : MonoBehaviour
 {
     [Min(0)]
     [field: SerializeField] protected float _initialHP;
