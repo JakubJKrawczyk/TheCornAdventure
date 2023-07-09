be32c826d6de7790f640f369999285a51446e277
JakubKrawczyk <36759354+JakubJKrawczyk@users.noreply.github.com>
2023-07-09 19:07:10 +0200

    Update PlayerMovement2D.cs

--- Assets/Scripts/Player/PlayerMovement2D.cs
+++ Assets/Scripts/Player/PlayerMovement2D.cs
@@ -5,16 +5,45 @@ using UnityEngine;
 public class PlayerMovement2D : MonoBehaviour
 {
 
-    public CharacterController2D controller;
+   
 
+    public CharacterController2D controller;
+    private Animator animator;
+    private Rigidbody2D _rigidbody2D;
+
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+    }
 
     private void FixedUpdate()
     {
         float XInput = Input.GetAxisRaw("Horizontal");
-        float YInput = Input.GetAxisRaw("Vertical");
-        bool Edown = Input.GetKeyDown(KeyCode.E);
+Expand All
+	@@ -25,22 +16,5 @@ private void FixedUpdate()
         bool Fdown = Input.GetKeyDown(KeyCode.F);
         controller.Move(XInput, YInput == -1 ? true : false, YInput == 1 ? true : false);
         controller.IsWPressed = YInput == 1 ? true : false;
+
+        if (XInput != 0 && YInput <= 0) controller.isWalking = true;
+        else controller.isWalking = false;
+
+        Debug.Log($"isRolling: {controller._isRolling}");
+        // Obs³uga animacji
+        if (controller.IsWPressed && _rigidbody2D.velocity.y > 1) animator.Play("player_jump_up", 0);
+        else if (controller._jumpCount == 0 && _rigidbody2D.velocity.y < -1) animator.Play("player_jump_down", 0);
+        else if (controller.isWalking && !controller._isRolling && !controller.isCrouching && !Input.GetKey(KeyCode.E)) animator.Play("player_walk", 0);
+        else if (controller._isRolling) animator.Play("player_roll", 0);
+        else if (Input.GetKey(KeyCode.E) && !controller.isCrouching) animator.Play("player_shoot", 0);
+        else if (Input.GetKey(KeyCode.E) && controller.isCrouching) animator.Play("player_crouch_shoot", 0);
+        else if (controller.isCrouching && !controller.isWalking && !controller._isRolling) animator.Play("player_crouch", 0);
+        else if (controller.isCrouchWalking && controller.isWalking) animator.Play("player_crawl", 0);
+        else animator.Play("Idle", 0);
+        // koniec obs³ugi animacji
+
     }
+
 }
