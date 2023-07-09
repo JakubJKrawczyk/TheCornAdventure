06ea977daa4aad9dbc186756fe2a0dc6a62a18ea
JakubKrawczyk <36759354+JakubJKrawczyk@users.noreply.github.com>
2023-07-09 19:52:23 +0200

    Update CharacterController2D.cs

--- Assets/Scripts/Player/CharacterController2D.cs
+++ Assets/Scripts/Player/CharacterController2D.cs
@@ -1,21 +1,15 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 public class CharacterController2D : MonoBehaviour
-
-    
-        
-          
-    
-
-        public class CharacterController2D : MonoBehaviour
-  
 {
     //props
     public bool IsWPressed { get; set; }
     [Header("Character Information")]
     [Range(0,100)] public float HP;
     
+
     [Header("Basic Properties")]
     [SerializeField] private float _jumpForce = 400f;
     [Range(0, 1)][SerializeField] private float _crouchSpeed = .36f;
@@ -48,18 +42,16 @@ public class CharacterController2D : MonoBehaviour
     [Space]
 
 
-    
-        
-          
-    
-
-        public class BoolEvent : UnityEvent<bool> { }
-  
     public UnityEvent OnLandEvent;
+
     [System.Serializable]
     public class BoolEvent : UnityEvent<bool> { }
+
     public BoolEvent OnCrouchEvent;
     private bool _wasCrouching = false;
+
+
+
     private void Awake()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
@@ -67,36 +59,28 @@ public class CharacterController2D : MonoBehaviour
 
         if (OnLandEvent == null) OnLandEvent = new UnityEvent();
         if (OnCrouchEvent == null) OnCrouchEvent = new BoolEvent();
-
-    
-        
-          
         _actualCharacterSpeed = _characterSpeed;
-    
     }
     private void FixedUpdate()
     {
         bool wasGrounded = _grounded;
         _grounded = false;
-         //obs³uga przetrzymanego skoku
+         //obsługa przetrzymanego skoku
         if (!IsWPressed && _rigidbody2D.velocity.y > 4)
         {
             _rigidbody2D.velocity /= new Vector2(1f, 3f);
-
+            
         }
+        
 
-
-        //obs³uga okienka skoku
+        //obsługa okienka skoku
         if (!_groundCheck.IsTouchingLayers(_whatIsGround.value)) {
             _jumpWindowTimer -= Time.deltaTime;
         }
         if(_jumpWindowTimer != _jumpWindow && _groundCheck.IsTouchingLayers(_whatIsGround.value)) _jumpWindowTimer = _jumpWindow;
 
-    
-        
-          
-
         if(_jumpWindowTimer < 0) _jumpCount = 0;
+
         //Sprawdzam czy jestem na ziemi
         
             if(_groundCheck.IsTouchingLayers(_whatIsGround.value))
@@ -106,32 +90,24 @@ public class CharacterController2D : MonoBehaviour
                 {
                     _jumpCount = 1;
                     OnLandEvent.Invoke();
-
+                    
                 }
             }
-
-
-
+        
+            
+       
 
     }
 
     /// <summary>
-
-    
-          
-            
-    
-
-          public void Move(float move, bool crouch, bool jump)
-  
-    /// Funkcja poruszaj¹ca postaci¹.
+    /// Funkcja poruszająca postacią.
     /// </summary>
     /// <param name="move">Nadaje kierunek ruchu postaci; -1 w lewo; 1 w prawo; 0 stój</param>
-    /// <param name="crouch">Informuje kontroler, ¿e postaæ chce kucn¹æ</param>
-    /// <param name="jump">Informuje kontroler, ¿e postaæ chce podskoczyæ</param>
+    /// <param name="crouch">Informuje kontroler, że postać chce kucnąć</param>
+    /// <param name="jump">Informuje kontroler, że postać chce podskoczyć</param>
     public void Move(float move, bool crouch, bool jump)
     {
-        //sprawdzam czy nad g³ow¹ znajduje siê przeszkoda i nie pozwalam postaci wstaæ
+        //sprawdzam czy nad głową znajduje się przeszkoda i nie pozwalam postaci wstać
         if(!crouch)
         {
             if(_ceilingCheck.IsTouchingLayers(_whatIsGround.value))
@@ -139,29 +115,25 @@ public class CharacterController2D : MonoBehaviour
                 crouch = true;
             }
         }
-        // obs³uga kucania   
+
+        // obsługa kucania   
         if(crouch)
         {
             if (!_wasCrouching)
             {
-                if (move != 0) _triedRolling = true; // je¿eli podczas ruchu zacz¹³ kucaæ
+                if (move != 0) _triedRolling = true; // jeżeli podczas ruchu zaczął kucać
 
                 _wasCrouching = true;
                 isCrouching = true;
                 OnCrouchEvent.Invoke(true);
 
 
-
+                
             }
 
             if(!_isRolling) move *= _crouchSpeed;
 
-    
-        
-          
-    
-public void Move(float move, bool crouch, bool jump)
-  
+
             if (_standingCollider != null)
             {
                 _standingCollider.enabled = false;
@@ -172,11 +144,11 @@ public void Move(float move, bool crouch, bool jump)
         }
         else
         {
-  
             if (_standingCollider != null)
             {
                 _standingCollider.enabled = true;
             }
+
             if (_wasCrouching)
             {
                 _wasCrouching = false;
@@ -184,19 +156,15 @@ public void Move(float move, bool crouch, bool jump)
                 OnCrouchEvent.Invoke(false);
                 _triedRolling = false;
 
-
+                
             }
             if (move != 0) isWalking = true;
             else isWalking = false;
         }
 
-        // nadanie prêdkoœci postaci
-
-    
-          
-       
-  
+        // nadanie prędkości postaci
         Vector3 targetVelocity = new Vector2(move * _actualCharacterSpeed, _rigidbody2D.velocity.y);
+
         _rigidbody2D.velocity = Vector3.SmoothDamp(_rigidbody2D.velocity, targetVelocity, ref _velocity, _movementSmoothing);
          // Korekcja kierunku patrzenia postaci
         if(move > 0 && !_facingRight)
@@ -206,15 +174,17 @@ public void Move(float move, bool crouch, bool jump)
         {
             Flip();
         }
-         //Obs³uga skakania
+         //Obsługa skakania
         if (jump && (_jumpCount > 0))
         {
             _grounded = false;
             _rigidbody2D.AddForce(new Vector2(0f, _jumpForce));
             _jumpCount--;
         }
-        // Obs³uga toczenia siê
-        if (_triedRolling && move == 0) _triedRolling = false; // je¿eli w czasie toczenia prêdkoœæ ruchu spad³a do 0
+
+        // Obsługa toczenia się
+        if (_triedRolling && move == 0) _triedRolling = false; // jeżeli w czasie toczenia prędkość ruchu spadła do 0
+
         if(move != 0 && crouch && _triedRolling)
         {
             Roll(true);
@@ -224,11 +194,11 @@ public void Move(float move, bool crouch, bool jump)
             Roll(false);
         }
 
-
+        
     }
 
-
-    // funkcja pozwalaj¹ca na toczenie postaci
+    
+    // funkcja pozwalająca na toczenie postaci
     private void Roll(bool isRolling)
     {
         if (isRolling)
@@ -246,9 +216,12 @@ public void Move(float move, bool crouch, bool jump)
     {
         // Switch the way the player is labelled as facing.
         _facingRight = !_facingRight;
+
         // Multiply the player's x local scale by -1.
         Vector3 theScale = transform.localScale;
         theScale.x *= -1;
         transform.localScale = theScale;
     }
+
+
 }
