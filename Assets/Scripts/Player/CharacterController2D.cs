d1078ebde8a59099adc75d8d6fa53b7c2e9eda7b
JakubKrawczyk <36759354+JakubJKrawczyk@users.noreply.github.com>
2023-07-09 19:10:36 +0200

    Update CharacterController2D.cs

--- Assets/Scripts/Player/CharacterController2D.cs
+++ Assets/Scripts/Player/CharacterController2D.cs
@@ -1,17 +1,21 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.ShaderGraph.Drawing;
 using UnityEngine;
 using UnityEngine.Events;
 public class CharacterController2D : MonoBehaviour
+
+    
+        
+          
+    
+
+        public class CharacterController2D : MonoBehaviour
+  
 {
     //props
     public bool IsWPressed { get; set; }
     [Header("Character Information")]
     [Range(0,100)] public float HP;
     
-
     [Header("Basic Properties")]
     [SerializeField] private float _jumpForce = 400f;
     [Range(0, 1)][SerializeField] private float _crouchSpeed = .36f;
@@ -24,59 +28,80 @@ public class CharacterController2D : MonoBehaviour
     [SerializeField] private float _rollSpeed = 10f;
     [Range(0,2f)][SerializeField] private float _jumpWindow = 0.07f;
     private bool _triedRolling = false;
-    private bool _isRolling = false;
+    private Animator animator;
+    private Rigidbody2D _rigidbody2D;
     private float _actualCharacterSpeed;
     private float _jumpWindowTimer = 0f;
-    private Rigidbody2D _rigidbody2D;
     private bool _facingRight = true;
-    const float _groundedRadius = .1f;
     private bool _grounded;
-    const float _ceilingRadius = .2f;
     private Vector3 _velocity = Vector3.zero;
-    private float _jumpCount = 1; 
+    internal float _jumpCount = 1;
+
+    //stany postaci
+
+    internal bool _isRolling = false;
+    internal bool isWalking = false;
+    internal bool isCrouchWalking = false;
+    internal bool isCrouching = false;
+
     [Header("Events")]
     [Space]
 
 
-    public UnityEvent OnLandEvent;
+    
+        
+          
+    
 
+        public class BoolEvent : UnityEvent<bool> { }
+  
+    public UnityEvent OnLandEvent;
     [System.Serializable]
     public class BoolEvent : UnityEvent<bool> { }
-
     public BoolEvent OnCrouchEvent;
     private bool _wasCrouching = false;
-
-
-
     private void Awake()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
 
         if (OnLandEvent == null) OnLandEvent = new UnityEvent();
         if (OnCrouchEvent == null) OnCrouchEvent = new BoolEvent();
+
+    
+        
+          
+    
+private void FixedUpdate()
+  
         _actualCharacterSpeed = _characterSpeed;
     }
     private void FixedUpdate()
     {
         bool wasGrounded = _grounded;
         _grounded = false;
-
+         //obsÂ³uga przetrzymanego skoku
         if (!IsWPressed && _rigidbody2D.velocity.y > 4)
         {
-            Debug.Log("Zmniejszanie prêdkoœci");
             _rigidbody2D.velocity /= new Vector2(1f, 3f);
-            
-        }
-        
 
-         //obs³uga okienka skoku
-        if(!_groundCheck.IsTouchingLayers(_whatIsGround.value)) {
+        }
+
+
+        //obsÂ³uga okienka skoku
+        if (!_groundCheck.IsTouchingLayers(_whatIsGround.value)) {
             _jumpWindowTimer -= Time.deltaTime;
         }
         if(_jumpWindowTimer != _jumpWindow && _groundCheck.IsTouchingLayers(_whatIsGround.value)) _jumpWindowTimer = _jumpWindow;
 
-        if(_jumpWindowTimer < 0) _jumpCount = 0;
+    
+        
+          
+    
 
+         private void FixedUpdate()
+  
+        if(_jumpWindowTimer < 0) _jumpCount = 0;
         //Sprawdzam czy jestem na ziemi
         
             if(_groundCheck.IsTouchingLayers(_whatIsGround.value))
@@ -86,20 +111,32 @@ public class CharacterController2D : MonoBehaviour
                 {
                     _jumpCount = 1;
                     OnLandEvent.Invoke();
-                    
+
                 }
             }
+
+
+
+
     }
 
     /// <summary>
-    /// Funkcja poruszaj¹ca postaci¹.
+
+    
+          
+            
+    
+
+          public void Move(float move, bool crouch, bool jump)
+  
+    /// Funkcja poruszajÂ¹ca postaciÂ¹.
     /// </summary>
-    /// <param name="move">Nadaje kierunek ruchu postaci; -1 w lewo; 1 w prawo; 0 stój</param>
-    /// <param name="crouch">Informuje kontroler, ¿e postaæ chce kucn¹æ</param>
-    /// <param name="jump">Informuje kontroler, ¿e postaæ chce podskoczyæ</param>
+    /// <param name="move">Nadaje kierunek ruchu postaci; -1 w lewo; 1 w prawo; 0 stÃ³j</param>
+    /// <param name="crouch">Informuje kontroler, Â¿e postaÃ¦ chce kucnÂ¹Ã¦</param>
+    /// <param name="jump">Informuje kontroler, Â¿e postaÃ¦ chce podskoczyÃ¦</param>
     public void Move(float move, bool crouch, bool jump)
     {
-        //sprawdzam czy nad g³ow¹ znajduje siê przeszkoda i nie pozwalam postaci wstaæ
+        //sprawdzam czy nad gÂ³owÂ¹ znajduje siÃª przeszkoda i nie pozwalam postaci wstaÃ¦
         if(!crouch)
         {
             if(_ceilingCheck.IsTouchingLayers(_whatIsGround.value))
@@ -107,44 +144,74 @@ public class CharacterController2D : MonoBehaviour
                 crouch = true;
             }
         }
-
-        // obs³uga kucania   
+        // obsÂ³uga kucania   
         if(crouch)
         {
             if (!_wasCrouching)
             {
-                if (move != 0) _triedRolling = true; // je¿eli podczas ruchu zacz¹³ kucaæ
+                if (move != 0) _triedRolling = true; // jeÂ¿eli podczas ruchu zaczÂ¹Â³ kucaÃ¦
 
                 _wasCrouching = true;
+                isCrouching = true;
                 OnCrouchEvent.Invoke(true);
+
+
+
             }
 
             if(!_isRolling) move *= _crouchSpeed;
 
-
+    
+        
+          
+    
+public void Move(float move, bool crouch, bool jump)
+  
             if (_standingCollider != null)
             {
                 _standingCollider.enabled = false;
             }
+
+            if (move != 0) isCrouchWalking = true;
+            else isCrouchWalking = false;
         }
         else
         {
+
+    
+        
+          
+    
+
+        public void Move(float move, bool crouch, bool jump)
+  
             if (_standingCollider != null)
             {
                 _standingCollider.enabled = true;
             }
-
             if (_wasCrouching)
             {
                 _wasCrouching = false;
+                isCrouching = false;
                 OnCrouchEvent.Invoke(false);
                 _triedRolling = false;
+
+
             }
+            if (move != 0) isWalking = true;
+            else isWalking = false;
         }
 
-        // nadanie prêdkoœci postaci
-        Vector3 targetVelocity = new Vector2(move * _actualCharacterSpeed, _rigidbody2D.velocity.y);
+        // nadanie prÃªdkoÅ“ci postaci
 
+    
+          
+            
+    
+
+          public void Move(float move, bool crouch, bool jump)
+  
+        Vector3 targetVelocity = new Vector2(move * _actualCharacterSpeed, _rigidbody2D.velocity.y);
         _rigidbody2D.velocity = Vector3.SmoothDamp(_rigidbody2D.velocity, targetVelocity, ref _velocity, _movementSmoothing);
          // Korekcja kierunku patrzenia postaci
         if(move > 0 && !_facingRight)
@@ -154,45 +221,29 @@ public class CharacterController2D : MonoBehaviour
         {
             Flip();
         }
-         //Obs³uga skakania
+         //ObsÂ³uga skakania
         if (jump && (_jumpCount > 0))
         {
             _grounded = false;
             _rigidbody2D.AddForce(new Vector2(0f, _jumpForce));
             _jumpCount--;
         }
-
-        // Obs³uga toczenia siê
-        if (_triedRolling && move == 0) _triedRolling = false; // je¿eli w czasie toczenia prêdkoœæ ruchu spad³a do 0
-
+        // ObsÂ³uga toczenia siÃª
+        if (_triedRolling && move == 0) _triedRolling = false; // jeÂ¿eli w czasie toczenia prÃªdkoÅ“Ã¦ ruchu spadÂ³a do 0
         if(move != 0 && crouch && _triedRolling)
         {
             Roll(true);
         }
-        else
-        {
-            Roll(false);
-        }
-        if(_velocity.x >-0.5 && _velocity.x < 0.5) 
+        else if(move == 0 || (_rigidbody2D.velocity.x > -1 || _rigidbody2D.velocity.x < 1))
         {
             Roll(false);
         }
 
+
     }
 
-    public void CrouchAnimation(bool crouch)
-    {
-        Animator animator = GetComponent<Animator>();
-        if(crouch)
-        {
-            animator.Play("PlayerCrouch", 0);
-        }
-        else
-        {
-            animator.Play("PlayerStandUp", 0);
-        }
-    }
-    // funkcja pozwalaj¹ca na toczenie postaci
+
+    // funkcja pozwalajÂ¹ca na toczenie postaci
     private void Roll(bool isRolling)
     {
         if (isRolling)
@@ -210,12 +261,9 @@ public class CharacterController2D : MonoBehaviour
     {
         // Switch the way the player is labelled as facing.
         _facingRight = !_facingRight;
-
         // Multiply the player's x local scale by -1.
         Vector3 theScale = transform.localScale;
         theScale.x *= -1;
         transform.localScale = theScale;
     }
-
-
 }
