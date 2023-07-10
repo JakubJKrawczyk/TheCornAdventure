f4c4649a4a1ab4b972c8a55026e92ccded5ee6d5
JakubKrawczyk <zrespawnieni@gmail.com>
2023-07-10 09:12:00 +0200

    MVP map set up

--- Assets/liftPlatform.cs
+++ Assets/liftPlatform.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.PackageManager;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class liftPlatform : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Animator m_Animator;
+    public bool isUp = true;
+    [SerializeField] private GameObject Player;
+    private void Awake()
     {
-        
+        m_Animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LiftDown()
     {
-        
+        if (isUp)
+        {
+            m_Animator.Play("LiftDown", 0);
+            isUp = false;
+            Debug.Log(isUp);
+        }
     }
+
+    public void LiftUp()
+    {
+        if(!isUp)
+        {
+            m_Animator.Play("LiftUp", 0);
+            isUp = true;
+            Debug.Log(isUp);
+
+        }
+    }
+
+   
 }
