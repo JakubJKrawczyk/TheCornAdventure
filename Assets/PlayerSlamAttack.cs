311978805784b7a36929687fd478d5b6d547201c
MB <62397807+M-Bujak@users.noreply.github.com>
2023-07-11 20:46:50 +0200

    Created scripts for stomping enemies

--- Assets/PlayerSlamAttack.cs
+++ Assets/PlayerSlamAttack.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerSlamAttack : MonoBehaviour
 {
+    [SerializeField] private float _damage;
+
     private WeightController _weightController;
     private CharacterController2D _characterController;
 
@@ -11,5 +13,13 @@ public class PlayerSlamAttack : MonoBehaviour
     {
         _weightController = GetComponent<WeightController>();
         _characterController = GetComponent<CharacterController2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.TryGetComponent(out EnemyStompWeakPoint enemyStompWeakPoint))
+        {
+            enemyStompWeakPoint.ReceiveDamage(_damage);
+        }
     }
 }
