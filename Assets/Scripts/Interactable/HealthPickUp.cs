7fe2a74cdc21a1a691a4e96bf4abec44b12dcd5f
JakubKrawczyk <zrespawnieni@gmail.com>
2023-07-12 17:02:43 +0200

    Base class added
    Base class PickUp has been added. HealthPickup and AmmoPickup are nheritancing after this class instead of MonoBehavior which is attached to Base Class PickUp.
    I also modified the code of this 2 classes and i left only unique fields.

--- Assets/Scripts/Interactable/HealthPickUp.cs
+++ Assets/Scripts/Interactable/HealthPickUp.cs
@@ -1,57 +1,17 @@
+using Assets.Scripts.Base_Classes;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthPickUp : MonoBehaviour
+public class HealthPickUp : PickUp
 {
     [Range(-1, 5)] public int HealingAmount = 1;
-    private float starty = 0;
-    private float endy = 0;
-    private float speed = 0.1f;
-    private bool goingup = true;
-    public bool ShouldFloat = true;
-    private Rigidbody2D rb;
+    
+    
 
 
-    void Start()
-    {
-        speed = Random.Range(0.2f, 0.4f);
-        rb = GetComponent<Rigidbody2D>();
-        if (ShouldFloat)
-        {
-            speed = 0.4f;
-            starty = transform.position.y - 0.5f;
-            endy = transform.position.y + 0.5f;
-            rb.velocity = new Vector2(0, speed);
-        }
-    }
-    private void Update()
-    {
-        if (ShouldFloat)
-        {
-            if ((rb.position.y - endy >= -0.05) && goingup)
-            {
-                GoDown();
-                goingup = false;
-            }
-            else if ((rb.position.y - starty <= 0.05) && !goingup)
-            {
-                GoUp();
-                goingup = true;
-            }
-        }
-    }
-    void OnDisable()
-    {
-
-    }
-    private void GoUp()
-    {
-        rb.velocity = new Vector2(0, speed);
-    }
-    private void GoDown()
-    {
-        rb.velocity = new Vector2(0, -speed);
-    }
+    
+    
+    
 
 }
