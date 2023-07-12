f52aa7fd6e76a641a51d3e0ed281ace3b0accadd
MB <62397807+M-Bujak@users.noreply.github.com>
2023-07-12 19:02:04 +0200

    Changed attack name

--- Assets/Scripts/Player/PlayerSlamAttack.cs
+++ Assets/Scripts/Player/PlayerSlamAttack.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerSlamAttack : MonoBehaviour
+public class PlayerStompAttack : MonoBehaviour
 {
     [SerializeField] private float _damage;
 
